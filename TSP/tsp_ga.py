7eb596b019accaa3dc1b492bca058bfd9cecf5ec
jinu2ID <jj559@drexel.edu>
2016-02-28 12:45:20 -0500

    added DEAP tools

--- TSP/tsp_ga.py
+++ TSP/tsp_ga.py
@@ -45,7 +45,6 @@ def shortest(tours):
 alltours = itertools.permutations # The permutation function is already defined in the itertools module
 numbersList = range(30)
 cities = set(numbersList)
-print cities
 distances = readDistances("30cities.txt")
 
 def total_distance(tour):
@@ -79,5 +78,29 @@ creator.create("Individual", list, fitness=creator.FitnessMin)
 toolbox.register("indices", numpy.random.permutation, len(cities))
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+toolbox.register("mate", tools.cxOrdered)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
+
+def create_tour(individual):
+	return [list(cities)[e] for e in individual]
+
+def evaluation(individual):
+    '''Evaluates an individual by converting it into 
+    a list of cities and passing that list to total_distance'''
+    return (total_distance(create_tour(individual)),)
+
+toolbox.register("evaluate", evaluation)
+
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+pop = toolbox.population(n=100)
+
+result,log =  algorithms.eaSimple(pop, toolbox, cxpb=0.8, mutpb=0.2, ngen=400, verbose=False)
+
+best_individual = tools.selBest(result, k=1)[0]
+print('Fitness of the best individual: ', evaluation(best_individual)[0])
+print best_individual
+
 
 
